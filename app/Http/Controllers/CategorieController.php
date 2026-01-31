bca0929de5fd08ef4618f5b7e5f914e3d0d41a86
youssef faouzi <yj1984515@gmail.com>
2026-01-31 15:42:06 +0100

    feat:ajout function de index avec route

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\categories ;
+
+class CategorieController extends Controller
+{
+    public function index(){
+        $categories = categories::all();
+        return view('categories.index' ,compact('categories'));
+    }
+}
